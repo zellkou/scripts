530cb28e986169ffe0a435fb40c9fc645d5e0423
zellkou <zellkoubarzhad@gmail.com>
2020-07-19 16:28:16 -0400

    added metadata.sh

---
+++ metadata.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for i in *.mp4;
+  do
+  # remove file extension
+    a="${i%.*}"
+    b=$a.1.mp4
+  # replace . with space in title
+    f="$(echo "$a" | sed 's/\./\ /g')";
+  # debugging echos
+    echo $i
+    echo $a
+    echo $f
+    echo $b
+  # edit metadata
+    ffmpeg -i $i -codec copy -metadata title="$f" $b
+    mv $b $i
+done
