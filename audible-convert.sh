780ffab0b2b6108ddf559c9af6d01565aa8e24a4
zellkou <zellkoubarzhad@gmail.com>
2020-07-31 21:45:22 -0400

    update

--- audible-convert.sh
+++ audible-convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in *.aax;
-    do ffmpeg -activation_bytes c61f0607 -i "$i" -vn -c:a copy "${i%.*}.m4b";
+    do ffmpeg -activation_bytes xxxxxxxx -i "$i" -vn -c:a copy "${i%.*}.m4b";
 done
 mv ~/Downloads/aax/*.m4b ~/Music/audibooks
--- pihole.sh
+++ pihole.sh
@@ -3,5 +3,5 @@
 # This adds domains to pihole list
 echo "Input domain to banish";
 read i;
-ssh -t needle "echo $i >> ~/Documents/adlist.txt";
+ssh -t pihole "echo $i >> ~/Documents/adlist.txt";
 exit
